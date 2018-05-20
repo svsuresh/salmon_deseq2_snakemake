2cd987234b9d113433d3d11e1a7948e2f0f66a81
suresh <12555651+svsuresh@users.noreply.github.com>
2018-05-20 15:54:34 +0530

    Add files via upload

--- scripts/salmon_deseq2.R
+++ scripts/salmon_deseq2.R
@@ -13,24 +13,23 @@ suppressMessages(library(pheatmap))
 suppressMessages(library(regionReport))
 
 # Store DESeq2 results
-dir.create('results', showWarnings = FALSE, recursive = TRUE)
 dir.create('results/salmon_deseq2_results', showWarnings = FALSE, recursive = TRUE)
 
 # Load sample information
 
-samples=data.frame(samples=col_concat(str_split_fixed(list.files("salmon"),"_",5)[,2:3], sep = "_"), condition=str_split_fixed(list.files("salmon"),"_",5)[,2])
+samples=data.frame(samples=col_concat(str_split_fixed(list.files("./results/salmon"),"_",5)[,2:3],"_"), condition=str_split_fixed(list.files("./results/salmon"),"_",5)[,2])
 row.names(samples)=samples[,1]
 
 ## Deseq2 workflow
-files=file.path("salmon",list.files("salmon"),"quant.sf")
-tx2gene=read.csv("reference/chr22_ucsc_grch38_refseq_txn2gene.tsv", sep = "\t",stringsAsFactors = F, header=F)
 
+files=file.path("results/salmon",list.files("results/salmon"),"quant.sf")
+
+tx2gene=read.csv("reference/t2gene.dedup.tsv", sep = "\t",stringsAsFactors = F, header=F)
 salmon_data <- tximport(files, type="salmon", tx2gene=tx2gene)
 
 ddsTxi <- DESeqDataSetFromTximport(salmon_data,
                                    colData = samples ,
                                    design = ~ condition)
-counts(ddsTxi)
 
 ## Filter transcripts with less than 10 counts
 keep <- rowSums(counts(ddsTxi)) >= 10
@@ -108,37 +107,37 @@ save.image("results/salmon_deseq2_results/salmon_results.Rdata")
 
 
 ## Wasabi and Sleuth workflow
-# dir.create("results/sleuth_results")
+dir.create("results/sleuth_results")
 # # Wasabi workflow
-# sfdirs <- file.path("salmon", c(list.files("salmon")))
-# prepare_fish_for_sleuth(sfdirs)
-
+sfdirs <- file.path("results/salmon", c(list.files("results/salmon")))
+sfdirs
+prepare_fish_for_sleuth(sfdirs)
 ## Preparation for sleuth
-# sfdata=data.frame(sample=list.files("salmon"), path=sfdirs, condition=samples$condition, stringsAsFactors = F)
-# design = ~condition
-# names(tx2gene)=c("target_id","HGNC")
-# so <- sleuth_prep(sfdata, design, target_mapping = tx2gene,num_cores = 1)
+sfdata=data.frame(sample=list.files("results/salmon"), path=sfdirs, condition=samples$condition, stringsAsFactors = F)
+ design = ~condition
+ names(tx2gene)=c("target_id","HGNC")
+ so <- sleuth_prep(sfdata, design, target_mapping = tx2gene,num_cores = 1)
 #
 # # Sleuth fit
-# so <- sleuth_fit(so)
+ so <- sleuth_fit(so)
 #
 # # Extract  expression data
-# oe <- sleuth_wt(so, 'conditiontumor')
+ oe <- sleuth_wt(so, 'conditiontumor')
 #
 # # Sleuth results as data frame
-# sleuth_results_oe=sleuth_results(oe, 'conditiontumor', show_all = TRUE)
+ sleuth_results_oe=sleuth_results(oe, 'conditiontumor', show_all = TRUE)
 #
-#
+
 # # Remove rows with no
-# sloe=sleuth_results_oe[complete.cases(sleuth_results_oe),]
-# write.csv(sloe, "results/sleuth_results/sleuth_expression_results.txt", sep="\t")
+sloe=sleuth_results_oe[complete.cases(sleuth_results_oe),]
+write.csv(sloe, "results/sleuth_results/sleuth_expression_results.txt", sep="\t")
 #
 # # Merge gene names from tx2gene object and order by qvalue
-# mer_sloe=merge(sloe, tx2gene, all.x=T)
-# mer_sloe[order(mer_sloe$qval),]
+ mer_sloe=merge(sloe, tx2gene, all.x=T)
+ mer_sloe[order(mer_sloe$qval),]
 #
 # # Write the results to hard disk
-# write.csv(sloe, "results/sleuth_results/sleuth_expression_results_merged.txt", sep="\t")
+ write.csv(sloe, "results/sleuth_results/sleuth_expression_results_merged.txt", sep="\t")
 #
 # # Save the workflow to HDD
-# save.image("results/sleuth_results/sleuth_results.Rdata")
+ save.image("results/sleuth_results/sleuth_results.Rdata")
